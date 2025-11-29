e38c915df99752cea5d43c11aef1ae498f987e65
mdesalvo <floating_point_9@msn.com>
2025-11-29 12:01:34 +0100

    Document registries

--- OWLSharp.Extensions/TIME/Abstractions/Registries/TIMEReferenceSystemRegistry.cs
+++ OWLSharp.Extensions/TIME/Abstractions/Registries/TIMEReferenceSystemRegistry.cs
@@ -20,21 +20,39 @@ using System.Collections.Generic;
 
 namespace OWLSharp.Extensions.TIME
 {
+    /// <summary>
+    /// Register for custom temporal reference systems (TRS)
+    /// </summary>
     public sealed class TIMEReferenceSystemRegistry : IEnumerable<TIMEReferenceSystem>
     {
         #region Properties
+        /// <summary>
+        /// Singleton instance of the register
+        /// </summary>
         public static TIMEReferenceSystemRegistry Instance { get; }
 
+        /// <summary>
+        /// Count of the registered temporal reference systems (TRS)
+        /// </summary>
         public static int TRSCount
             => Instance.TRS.Count;
 
+        /// <summary>
+        /// Enumerator to iterate the registered temporal reference systems (TRS)
+        /// </summary>
         public static IEnumerator<TIMEReferenceSystem> TRSEnumerator
             => Instance.TRS.Values.GetEnumerator();
 
+        /// <summary>
+        /// Internal dictionary of registered temporal reference systems (TRS)
+        /// </summary>
         internal Dictionary<string,TIMEReferenceSystem> TRS { get; set; }
         #endregion
 
         #region Ctors
+        /// <summary>
+        /// Initializes the singleton register instance with standard calendar and positional temporal reference systems (TRS)
+        /// </summary>
         static TIMEReferenceSystemRegistry()
         {
             Instance = new TIMEReferenceSystemRegistry
@@ -53,20 +71,32 @@ namespace OWLSharp.Extensions.TIME
         #endregion
 
         #region Interfaces
+        /// <summary>
+        /// Gets the enumerator on the registered temporal reference systems (TRS)
+        /// </summary>
         IEnumerator<TIMEReferenceSystem> IEnumerable<TIMEReferenceSystem>.GetEnumerator()
             => TRSEnumerator;
 
+        /// <summary>
+        /// Gets the enumerator on the registered temporal reference systems (TRS)
+        /// </summary>
         IEnumerator IEnumerable.GetEnumerator()
             => TRSEnumerator;
         #endregion
 
         #region Methods
+        /// <summary>
+        /// Registers the given temporal reference system (TRS)
+        /// </summary>
         public static void AddTRS(TIMEReferenceSystem trs)
         {
             if (trs != null && !Instance.TRS.ContainsKey(trs.ToString()))
                 Instance.TRS.Add(trs.ToString(), trs);
         }
 
+        /// <summary>
+        /// Checks if the given temporal reference system (TRS) is registered or not
+        /// </summary>
         public static bool ContainsTRS(RDFResource trsURI)
             => trsURI != null && Instance.TRS.ContainsKey(trsURI.ToString());
         #endregion
--- OWLSharp.Extensions/TIME/Abstractions/Registries/TIMEUnitTypeRegistry.cs
+++ OWLSharp.Extensions/TIME/Abstractions/Registries/TIMEUnitTypeRegistry.cs
@@ -20,21 +20,39 @@ using System.Collections.Generic;
 
 namespace OWLSharp.Extensions.TIME
 {
+    /// <summary>
+    /// Register for custom temporal unit types
+    /// </summary>
     public sealed class TIMEUnitTypeRegistry : IEnumerable<TIMEUnit>
     {
         #region Properties
+        /// <summary>
+        /// Singleton instance of the register
+        /// </summary>
         public static TIMEUnitTypeRegistry Instance { get; }
 
+        /// <summary>
+        /// Count of the registered temporal unit types
+        /// </summary>
         public static int UnitTypeCount
             => Instance.UnitTypes.Count;
 
+        /// <summary>
+        /// Enumerator to iterate the registered temporal unit types
+        /// </summary>
         public static IEnumerator<TIMEUnit> UnitTypeEnumerator
             => Instance.UnitTypes.Values.GetEnumerator();
 
+        /// <summary>
+        /// Internal dictionary of registered temporal unit types
+        /// </summary>
         internal Dictionary<string,TIMEUnit> UnitTypes { get; set; }
         #endregion
 
         #region Ctors
+        /// <summary>
+        /// Initializes the singleton register instance with standard OWL-TIME temporal unit types (and a couple of custom ones)
+        /// </summary>
         static TIMEUnitTypeRegistry()
         {
             Instance = new TIMEUnitTypeRegistry
@@ -62,20 +80,32 @@ namespace OWLSharp.Extensions.TIME
         #endregion
 
         #region Interfaces
+        /// <summary>
+        /// Gets the enumerator on the registered temporal unit types
+        /// </summary>
         IEnumerator<TIMEUnit> IEnumerable<TIMEUnit>.GetEnumerator()
             => UnitTypeEnumerator;
 
+        /// <summary>
+        /// Gets the enumerator on the registered temporal unit types
+        /// </summary>
         IEnumerator IEnumerable.GetEnumerator()
             => UnitTypeEnumerator;
         #endregion
 
         #region Methods
+        /// <summary>
+        /// Registers the given temporal unit type
+        /// </summary>
         public static void AddUnitType(TIMEUnit unitType)
         {
             if (unitType != null && !Instance.UnitTypes.ContainsKey(unitType.ToString()))
                 Instance.UnitTypes.Add(unitType.ToString(), unitType);
         }
 
+        /// <summary>
+        /// Checks if the given temporal unit type is registered or not
+        /// </summary>
         public static bool ContainsUnitType(RDFResource unitTypeURI)
             => unitTypeURI != null && Instance.UnitTypes.ContainsKey(unitTypeURI.ToString());
         #endregion
